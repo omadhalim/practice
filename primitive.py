0edd9cf746a7e2e5f406e2ace21381c5d13fe2a1
omadhalim <omadjonzokirjonov977@gmail.com>
2026-09-13 13:35:11 +0900

    learn python boolean

--- primitive.py
+++ primitive.py
@@ -26,3 +26,22 @@ print(f"the result (3): {result}")
 
 result = course.replace("FullStack", "MasterClass")
 print(f"the result (4): {result}")
+
+print("===== boolean =====")
+# functions > type() input() bool() int() str()
+
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY value
+# TRUTHY: True 100 -100 "MIT"
+# FALSY: False 0 "" None
+
+test_falsy = "" or False or None or 0
+print("test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
